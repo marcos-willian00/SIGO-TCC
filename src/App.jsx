4077392fd2f0633755fe9e7353c8ba25556fd884
Maria Eduarda Ferreira Batista <ferreirabatistam34@gmail.com>
2025-05-16 20:44:40 -0300

    feat: telas de recuperação de senha

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import Form from "./components/Form";
 import RecuperarSenha from "./components/RecuperarSenha";
+import CodigoRecuperacao from "./components/CodigoRecuperacao";
+import NovaSenha from "./components/NovaSenha";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Form />} />
       <Route path="/RecuperarSenha" element={<RecuperarSenha />} />
+      <Route path="/CodigoRecuperacao" element={<CodigoRecuperacao />} />
+      <Route path="/NovaSenha" element={<NovaSenha />} />
       <Route path="*" element={<Form />} />
     </Routes>
   );
--- src/components/RecuperarSenha.jsx
+++ src/components/RecuperarSenha.jsx
@@ -1,17 +1,30 @@
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
 function RecuperarSenha() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Aqui você pode adicionar lógica para enviar o email
+    navigate("/CodigoRecuperacao", { state: { email } });
+  };
+
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md flex flex-col h-[60vh]">
-        {/* Div do formulário */}
         <div className="flex-1 min-w-0">
           <h2 className="text-2xl font-bold mb-2 text-center text-[#2F9E41]">Recuperação de Conta</h2>
-          <div className="h-1 bg-[#2F9E41] w-70 mx-auto mb-6 rounded-full"></div>
-          <form>
+          <div className="h-1 bg-[#2F9E41] w-24 mx-auto mb-6 rounded-full"></div>
+          <form onSubmit={handleSubmit}>
             <div className="mb-6">
               <label htmlFor="email" className="block font-bold text-black-600 mb-3 mt-15">Email</label>
               <input
                 type="email"
                 id="email"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
                 placeholder="Digite seu email para recuperar a senha"
                 className="w-full p-3 border border-[#2F9E41] rounded-md focus:outline-none focus:ring-2 focus:ring-[#2F9E41]"
               />
