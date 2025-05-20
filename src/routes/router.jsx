9423c284709b121e7b9cddd304715c79846a74cc
Marcos Willian <marcoswillian983@gmail.com>
2025-05-20 14:52:23 -0300

    :sparkles: feat: add SignUp page and route

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -3,12 +3,14 @@ import RecuperarSenha from '../components/RecuperarSenha';
 import CodigoRecuperacao from '../components/CodigoRecuperacao';
 import NovaSenha from '../components/NovaSenha';
 import Login from '../pages/auth/Login';
+import SignUp from '../pages/auth/SignUp';
 import MainpageAluno from '../pages/aluno/MainpageAluno';
 
 function Router() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/cadastro" element={<SignUp />} />
       <Route path="/recupera-senha" element={<RecuperarSenha />} />
       <Route path="/codigo-de-recuperacao" element={<CodigoRecuperacao />} />
       <Route path="/nova-senha" element={<NovaSenha />} />
